44871e4f7f421d40f276079cef9cf952d7537df9
Ivan <ivansanchez_1988@hotmail.com>
2017-02-08 13:03:52 -0500

    Prueba de reconocimiento de audio en espanol de google speech usando formato flac sample rate 44100 y un solo canal

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
